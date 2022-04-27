7de21cdbf2febc72d33050666dcf458227a8520b
hono3bono3 <97086033+hono3bono3@users.noreply.github.com>
2022-04-27 09:30:15 +0900

    delete dist

--- dist/callbacks.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-{
-    const map = (arr, callback) => {
-        return callback(arr);
-    };
-    console.log(map([1, 2, 3, 4, 5, 6], (arg) => {
-        return arg.filter(a => (a % 2) !== 0);
-    }));
-}
--- dist/destructuringAssignment.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const fooBar = {
-    foo: 'foo',
-    bar: 123,
-};
-const { foo, bar } = fooBar;
-console.log(foo);
-console.log(bar);
